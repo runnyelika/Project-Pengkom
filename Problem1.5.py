64922e83fc9faad57a02099fb1096ebb933f72d8
Hughie Raymonelika <71703724+runnyelika@users.noreply.github.com>
2020-10-11 22:01:09 +0700

    Add files via upload

--- Problem1.5.py
+++ Problem1.5.py
@@ -1,7 +1,7 @@
 #PROBLEM 1.5
 
-x = int(input("Harga sebelum diskon : "))
+x = int(input("Harga sebelum diskon : ))
 z = int(input("Besar diskon (dalam persen) :"))
 
 y = (100-z) / 100 * x
-print("Harga setelah diskon adalah "+str(y))
+print("Harga setelah diskon adalah "+str(y)
